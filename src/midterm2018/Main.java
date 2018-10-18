00320fc58a9a32d8f81e115cd60dd78524e67056
brianxautumn <shred@brianautumn.com>
2018-10-17 20:17:39 -0700

    initial commit

---
+++ src/midterm2018/Main.java
@@ -0,0 +1,44 @@
+package midterm2018;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String question1and2 = "GET /add?a=3&b=4 HTTP/1.1\n"
+            + "Host: localhost:1298\n"
+            + "Connection: keep-alive\n"
+            + "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36\n"
+            + "Accept: image/webp,image/apng,image/*,*/*;q=0.8\n"
+            + "Referer: http://localhost:1298/\n"
+            + "Accept-Encoding: gzip, deflate, br\n"
+            + "Accept-Language: en-US,en;q=0.9,es;q=0.8\n";
+
+        String question3 = "{\n"
+            + "    “task” : “inc”,\n"
+            + "    “num” : 3\n"
+            + "}\n";
+
+        String question4and5 = "To opt out, you’ll need to enter the Settings menu by clicking the three vertical dots, all the way in the upper right corner of the browser. From there, you’ll need to enter the Advanced settings at the very bottom and find the “Allow Chrome sign in” toggle, then turn it to off. Doing so lets you sign into Google services like Gmail and Maps without signing into the Chrome browser itself.";
+
+
+        System.out.println(question1and2);
+        System.out.println(question3);
+        System.out.println(question4and5);
+
+        // print each answer at the end
+
+        // Question 1
+        // Return the Host
+
+        // Question 2
+        // return sum of a and b
+
+        // Question 3
+        // convert to java object, increment num, convert back to json and return
+
+        // Question 4
+        // return unique words
+
+        // Question 5
+        // return 3rd most common word
+    }
+}
