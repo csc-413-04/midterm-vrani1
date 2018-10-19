e7708f54c7cbfc486365ec01bcc543bb7851b3aa
Vanita <vrani@mail.sfsu.edu>
2018-10-18 21:49:15 -0700

    midterm

--- src/midterm2018/Main.java
+++ src/midterm2018/Main.java
@@ -1,6 +1,21 @@
 package midterm2018;
 
+import com.google.gson.*;
+import java.util.*;
+
 public class Main {
+     static void countWords(final String text) {
+        // used https://stackoverflow.com/questions/22981210/counting-unique-words-in-a-string-without-using-an-array
+        Scanner read = new Scanner(text);
+        Set<String> uniqueWords = new HashSet<String>();
+
+        while (read.hasNext()) {
+            uniqueWords.add(read.next());
+        }
+        read.close();
+        System.out.println("Question 4: " + uniqueWords.size());
+        uniqueWords.remove("the");
+    }
 
     public static void main(String[] args) {
         String question1and2 = "GET /add?a=3&b=4 HTTP/1.1\n"
@@ -13,8 +28,8 @@ public class Main {
             + "Accept-Language: en-US,en;q=0.9,es;q=0.8\n";
 
         String question3 = "{\n"
-            + "    “task” : “inc”,\n"
-            + "    “num” : 3\n"
+            + "    \"task\" : \"inc\",\n"
+            + "    \"num\" : 3\n"
             + "}\n";
 
         String question4and5 = "To opt out, you’ll need to enter the Settings menu by clicking the three vertical dots, all the way in the upper right corner of the browser. From there, you’ll need to enter the Advanced settings at the very bottom and find the “Allow Chrome sign in” toggle, then turn it to off. Doing so lets you sign into Google services like Gmail and Maps without signing into the Chrome browser itself.";
@@ -28,17 +43,40 @@ public class Main {
 
         // Question 1
         // Return the Host
+        String[] parts = question1and2.split("\n");
+        String q1 = parts[1];
+        String[] q = q1.split(": ");
+        String host = q[1];
+        System.out.println("\nQuestion 1: " + host);
 
         // Question 2
         // return sum of a and b
+        String[] q2 = parts[0].split(" ");
+        String[] q21 = q2[1].split("\\?");
+        String[] param = q21[1].split("&");
+        String aaa[] = param[0].split("=");
+        String bbb[] = param[1].split("=");
+        int aResult = Integer.parseInt(aaa[1]);
+        int bResult = Integer.parseInt(bbb[1]);
+        int sum = aResult + bResult;
+        System.out.println("Question 2: " + sum);
 
         // Question 3
         // convert to java object, increment num, convert back to json and return
+        Gson gson = new Gson();
+        Question3 obj = gson.fromJson(question3, Question3.class);
+        obj.incrementNum();
+        String jsonString = gson.toJson(obj);
+        System.out.println("Question 3: " + jsonString);
 
         // Question 4
         // return unique words
+        countWords(question4and5);
 
         // Question 5
         // return 2nd most common word
+        // unset (get rid of) a key
+        System.out.println("Question 5: ");
+
     }
 }
---
+++ src/midterm2018/Question3.java
@@ -0,0 +1,26 @@
+package midterm2018;
+
+public class Question3 {
+    private String task;
+    private int num;
+
+    public String getTask() {
+        return task;
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public void incrementNum() {
+        num++;
+    }
+
+    public void setTask(String task) {
+        this.task = task;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+}
